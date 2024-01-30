afa204a24fd5151df19bfb9a1dce5184f37123b6
Eliane-M <125857083+Eliane-M@users.noreply.github.com>
2024-01-30 10:55:43 +0200

    sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
 --For the AirBnB clone
 
-CREATE DATABASE IF NOT EXIST hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT PRIVILEGES ON performance_schema.* TO 'hbnb_dev'@'localhost';
