6bc828f336c020b50886d9141e945a696b0a1c93
Eliane_M <munezeroeliane761@gmail.com>
2024-02-02 02:30:39 -0800

    DBStorage - User

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,10 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, String
+
+
+Base = declarative_base()
 
 
 class BaseModel:
@@ -13,7 +17,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -21,6 +24,8 @@ class BaseModel:
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
+
+            
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -31,6 +36,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -42,3 +48,9 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+
+    id = Column(String(60), unique=True), nullable=False, primary_key=True
+
+    created_at = Column(String(datetime.utcnow())), nullable=False
+    
+    updated_at = Column(String(datetime,utcnow())), nullable=False
