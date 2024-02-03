b1a86ff94c71af16bb8b73585d406a9b82542908
Eliane-M <125857083+Eliane-M@users.noreply.github.com>
2024-02-03 22:54:57 +0200

    DBStorage - Amenity

--- models/amenity.py
+++ models/amenity.py
@@ -7,4 +7,15 @@ class Amenity(BaseModel, Base):
     """defines the table for amenities"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities=
+    place_amenities= Table(
+                            "place_amenity", Base.metadata,
+                            Column('place_id',
+                                   String(60),
+                                   ForeignKey('places.id'),
+                                   primary_key=True,
+                                   nullable=False),
+                            Column('amenity_id',
+                                   String(60),
+                                   ForeignKey('amenities.id'),
+                                   primary_key=True,
+                                   nullable=False))
