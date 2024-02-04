bf64959b20037b0bfc1af34980a08281cb955470
Lindah <l.nyambura@alustudent.com>
2024-02-04 19:43:54 +0300

    amenities

--- models/amenity.py
+++ models/amenity.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy import Column, String, Table, ForeignKey
+from os import getenv
+
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """defines the table for amenities"""
-    __tablename__ = "amenities"
+    '''amenity class'''
+    __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = Table(
-        "place_amenity", Base.metadata,
-        Column('place_id',
-               String(60),
-               ForeignKey('places.id'),
-               primary_key=True,
-               nullable=False),
-        Column('amenity_id',
-               String(60),
-               ForeignKey('amenities.id'),
-               primary_key=True,
-               nullable=False)
-    )
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship('Place', secondary="place_amenity")
