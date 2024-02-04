ca37fb3021d68d20d6ae9af879341283e04cec18
Eliane-M <125857083+Eliane-M@users.noreply.github.com>
2024-02-04 14:27:11 +0200

    task 2

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 """ Module for testing file storage"""
 import unittest
-from models.base_model import BaseModel
 from models import storage
 import os
 import pep8
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenities import Amenitites
+from models.place import Place
+from models.review import Review
+from models.engine.file_storage import FileStorage
 
 
 class test_fileStorage(unittest.TestCase):
