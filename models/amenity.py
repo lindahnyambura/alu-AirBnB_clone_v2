6492a0645c9e51830f103cbbbdb150c5e38cd90f
Eliane-M <125857083+Eliane-M@users.noreply.github.com>
2024-02-09 12:08:16 +0200

    amenity table

--- models/amenity.py
+++ models/amenity.py
@@ -11,16 +11,5 @@ class Amenity(BaseModel, Base):
     '''amenity class'''
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-
-    place_amenity = Table(
-                            "place_amenity", Base.metadata,
-                            Column('place_id',
-                                   String(60),
-                                   ForeignKey('places.id'),
-                                   primary_key=True,
-                                   nullable=False),
-                            Column('amenity_id',
-                                   String(60),
-                                   ForeignKey('amenities.id'),
-                                   primary_key=True,
-                                   nullable=False))
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship('Place', secondary="place_amenity")
