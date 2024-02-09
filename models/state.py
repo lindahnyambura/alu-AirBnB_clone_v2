8779cec9159b5bc111fe0ff9144a396df8781ea0
Eliane-M <125857083+Eliane-M@users.noreply.github.com>
2024-02-09 16:00:23 +0200

    state table

--- models/state.py
+++ models/state.py
@@ -13,7 +13,7 @@ class State(BaseModel, Base):
     __tablename__ = "states"
 
     name = Column(String(128), nullable=False)
-
+    id = Column(Integer, primary_key=True)
 
 if os.getenv('HBNB_TYPE_STORAGE') == 'db':
     cities = relationship('City', backref="state",
