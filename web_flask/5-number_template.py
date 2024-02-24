6812d8403e4f3e509265bd5c126567eba0fc2e1a
Lindah <l.nyambura@alustudent.com>
2024-02-24 09:01:11 +0300

    task4 pep, task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ a script that starts a Flask web application """
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -19,16 +20,14 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
-    """ return C """
+    """ def doc: return C with text """
     return 'c {}'.format(text.replace("_", " "))
 
 
 @app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
-    """
-    def doc: return python with text or default text
-    """
+    """ def doc: return python with text or default text """
     return 'Python {}'.format(text.replace("_", " "))
 
 
@@ -38,6 +37,11 @@ def number(n):
     def doc: return number if int else return not a number
     """
     return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    return render_template('5-number.html', number=n)
 
 
 if __name__ == "__main__":
